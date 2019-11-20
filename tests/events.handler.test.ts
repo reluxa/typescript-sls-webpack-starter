e935d5ea278fc969fca3a8a9d42af5dc78a7275f
reluxa <reluxa@gmail.com>
2019-11-20 23:18:00 +0100

    unit test enhanced

--- tests/events.handler.test.ts
+++ tests/events.handler.test.ts
@@ -1,6 +1,6 @@
 import { getEvent, } from "../src/events/events.lambda.handler"
 import { APIGatewayProxyEvent, APIGatewayEventRequestContext, Callback, Context, APIGatewayProxyResult } from "aws-lambda";
-import {DIContainer, Types} from "../src/common/container"
+import { DIContainer, Types } from "../src/common/container"
 import { IEventRepository, Event } from "../src/events/events.interface"
 import { User } from "../src/users/users.interface"
 import { injectable } from "inversify";
@@ -9,33 +9,33 @@ describe('getvent', function () {
   it('Should call repository when called', async function () {
 
     let proxyEvent: APIGatewayProxyEvent = {
-        body: null,
-        headers: {},
-        multiValueHeaders: {},
-        httpMethod: "GET",
-        isBase64Encoded: false,
-        path: "/api/events",
-        pathParameters: { id:"1"},
-        queryStringParameters: null,
-        multiValueQueryStringParameters: null,
-        stageVariables: null,
-        resource: "event",
-        requestContext: <APIGatewayEventRequestContext>{},
+      body: null,
+      headers: {},
+      multiValueHeaders: {},
+      httpMethod: "GET",
+      isBase64Encoded: false,
+      path: "/api/events",
+      pathParameters: { id: "1" },
+      queryStringParameters: null,
+      multiValueQueryStringParameters: null,
+      stageVariables: null,
+      resource: "event",
+      requestContext: <APIGatewayEventRequestContext>{},
     }
 
     @injectable()
     class MockRepository implements IEventRepository {
       getEvent(id: number): Promise<Event> {
         let ev: Event = {
-            name: "name",
-            id: "1",
-            venue: "Budapest",
-            owner: <User>{},
-            creation: new Date(),
+          name: "name",
+          id: "1",
+          venue: "Budapest",
+          owner: <User>{},
+          creation: new Date("2019-11-20T22:10:52.722Z"),
         }
         return Promise.resolve(ev);
-      } 
-     
+      }
+
       createEvent(event: Event): Promise<Event> {
         throw new Error("Method not implemented.");
       }
@@ -43,9 +43,9 @@ describe('getvent', function () {
 
     DIContainer.unbind(Types.IEventRepository);
     DIContainer.bind<IEventRepository>(Types.IEventRepository).to(MockRepository);
-    
+
     let response = await getEvent(proxyEvent, <Context>{}, <Callback<APIGatewayProxyResult>>{});
     expect((<APIGatewayProxyResult>response).statusCode).toEqual(200);
-    //expect((<APIGatewayProxyResult>response).body).toEqual('{"name":"name","id":"1","venue":"Budapest","owner":{},"creation":"2019-11-20T22:10:52.722Z"}');
+    expect((<APIGatewayProxyResult>response).body).toEqual('{"name":"name","id":"1","venue":"Budapest","owner":{},"creation":"2019-11-20T22:10:52.722Z"}');
   });
 });
